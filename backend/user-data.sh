20952198e10da173c0ba2a7b5598f5267c9cbc1d
Anthony <anthonyrov@gmail.com>
2025-03-24 20:45:42 +0100

    Update user-data.sh for backend instance setup
    - Added sudo to package installation commands for AWS CLI and Docker.
    - Included commands to connect to the ECR repository and pull the backend image.
    - Updated Docker run command to use the ECR image and specified environment file.
    - Enhanced user-data script for improved permissions and functionality.

--- backend/user-data.sh
+++ backend/user-data.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
 # System update
-apt update && apt upgrade -y
+sudo apt update && apt upgrade -y
+
+# Install AWS CLI
+sudo apt install -y awscli
 
 # Install Docker
-apt install -y docker.io
+sudo apt install -y docker.io
 systemctl start docker
 systemctl enable docker
 
 # Install Docker Compose (v2)
-mkdir -p /usr/local/lib/docker/cli-plugins
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 # Add the user to the docker group
-usermod -aG docker ubuntu
+sudo usermod -aG docker ubuntu
 
-# Download the Docker Compose configuration file and start the containers
-docker pull <votre-image-backend>
-docker run -d --name backend -p 3000:3000 <votre-image-backend>
+# Connect to the ECR repository
+AWS_ACCOUNT_ID="<AWS_ACCOUNT_ID>"
+REGION="eu-west-3"
+IMAGE_NAME="portfolio-backend"
+
+sudo aws ecr get-login-password --region $REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+
+IMAGE_TAG="latest"
+ECR_IMAGE="$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG"
+
+sudo docker stop backend || true
+sudo docker rm backend || true
+
+sudo docker pull $ECR_IMAGE
+
+sudo docker run -d \
+  --name backend \
+  -p 3000:3000 \
+  --env-file /home/ubuntu/.env \
+  $ECR_IMAGE
