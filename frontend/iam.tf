7f1c94fd4a3f23629bce9368376ed832e9785e8c
Anthony <anthonyrov@gmail.com>
2025-03-26 14:41:24 +0100

    Enhance IAM policies in iam.tf for CloudFront and GitHub Actions
    - Updated the S3 policy document to include a new statement for CloudFront read access with a more descriptive SID.
    - Added a new statement for GitHub Actions, granting permissions for object manipulation and bucket listing.
    - Improved resource references for clarity and consistency.

--- frontend/iam.tf
+++ frontend/iam.tf
@@ -3,14 +3,30 @@ resource "aws_cloudfront_origin_access_identity" "frontend" {
 }
 
 data "aws_iam_policy_document" "s3_policy" {
+  # Cloudfront autorisations
   statement {
-    sid = "1"
-    actions   = ["s3:GetObject"] # Allow read access to the bucket
+    sid = "CloudFrontReadAccess"
+    actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.frontend.arn}/*"]
 
     principals {
       type        = "AWS"
       identifiers = [aws_cloudfront_origin_access_identity.frontend.iam_arn]
+    }
+  }
+
+  # Github Actions autorisations
+  statement {
+    sid = "GitHubActionsWriteAccess"
+    actions   = ["s3:PutObject", "s3:DeleteObject", "s3:ListBucket"]
+    resources = [
+      aws_s3_bucket.frontend.arn,
+      "${aws_s3_bucket.frontend.arn}/*"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_user.github_actions.arn]
     }
   }
 }
@@ -22,8 +38,17 @@ resource "aws_s3_bucket_policy" "frontend" {
 
 data "aws_iam_policy_document" "github_actions" {
   statement {
-    actions   = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"]
-    resources = ["${aws_s3_bucket.frontend.arn}/*", aws_s3_bucket.frontend.arn]
+    actions   = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:DeleteObject",  
+      "s3:GetBucketLocation"
+    ]
+    resources = [
+      aws_s3_bucket.frontend.arn,
+      "${aws_s3_bucket.frontend.arn}/*"
+    ]
   }
 }
 
